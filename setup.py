8f470faa4fd77388d5f5bd5429605bee191eedc6
jgillick <j_gillick@yahoo.com>
2013-04-29 11:25:07 -0700

    setup for package installation

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='LendingClubAutoInvestor',
+    version='0.5',
+    author='Jeremy Gillick',
+    author_email='j_gillick@yahoo.com',
+    packages=[],
+    scripts=['bin/investor.py'],
+    url='https://github.com/jgillick/LendingClubAutoInvestor',
+    license='LICENSE.txt',
+    description='A simple tool that will watch your LendingClub account and automatically invest cash as it becomes available.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "python-daemon >= 1.6",
+        "requests >= 1.2.0",
+        "beautifulsoup4 >= 4.1.3",
+        "html5lib >= 0.95",
+        "argparse >= 1.2.1"
+    ],
+)
